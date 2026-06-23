3ccd81015a335173aa6835c25f0602152e7031ee
Rajat Rauthan <rajat.rauthan@accenture.com>
2026-06-24 00:52:41 +0530

    last commit of the day

--- env/dev/keyvault.tf
+++ env/dev/keyvault.tf
@@ -9,30 +9,25 @@ resource "azurerm_key_vault" "keyvaultdetails" {
   purge_protection_enabled    = false
 
   sku_name = "standard"
-
-  access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
-
-    key_permissions = [
-      "Get",
-    ]
-
-    secret_permissions = [
-      "Get",
-      "List",
-      "Set",
-      "Delete",
-      "Purge"
-    ]
-
-    storage_permissions = [
-      "Get",
-    ]
   }
-}
 
 data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault_access_policy" "terraform_policy" {
+
+  key_vault_id = azurerm_key_vault.keyvaultdetails.id
+
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  object_id = data.azurerm_client_config.current.object_id
+
+  secret_permissions = [
+    "Get",
+    "List",
+    "Set",
+    "Delete",
+    "Purge"
+  ]
+}
 
 resource "random_password" "randompass" {
   length  = 20
@@ -44,12 +39,12 @@ resource "azurerm_key_vault_secret" "username" {
   name         = "linux-admin-username"
   value        = "azureadmin"
   key_vault_id = azurerm_key_vault.keyvaultdetails.id
-  depends_on = [ azurerm_key_vault.keyvaultdetails ]
+  depends_on = [azurerm_key_vault_access_policy.terraform_policy]
 }
 
 resource "azurerm_key_vault_secret" "password" {
   name         = "linux-admin-password"
   value        = random_password.randompass.result
   key_vault_id = azurerm_key_vault.keyvaultdetails.id
-  depends_on = [ azurerm_key_vault.keyvaultdetails ]
+  depends_on = [azurerm_key_vault_access_policy.terraform_policy]
 }
